eaf5e7d4c9f59ac23247c096d4c7b30f924a47b5
sunnyjovita <55756082+sunnyjovita@users.noreply.github.com>
2020-03-24 17:43:38 +0700

    Update Driver_class.java

--- Driver_class.java
+++ Driver_class.java
@@ -18,17 +18,30 @@ public class Driver_class {
 //        System.out.println(acc.Transfer("milo", 100000));
 
         Date d1 = new Date(22, 2, 2000);
-
         System.out.println(d1);
-
         d1.setDay(2);
         d1.setMonth(3);
         d1.setYear(1997);
-
         System.out.println(d1);
-
         System.out.println(d1.toString());
 
+        Time t1 = new Time(5, 23, 2);
+
+        System.out.println(t1);
+
+        t1.setHour(12);
+        t1.setMinute(50);
+        t1.setSecond(32);
+
+        System.out.println(t1);
+
+        t1.setTime(15, 23, 2);
+
+        System.out.println(t1);
+
+        System.out.println(t1.toString());
+
+ 
 
 
 
